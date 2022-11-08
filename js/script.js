717debbd6ab7b37cc346385de97bbf24733aae8a
sossut <mikki.alakari@elisanet.fi>
2022-11-08 17:17:48 +0200

    eilen ja huomenna napit

--- js/script.js
+++ js/script.js
@@ -7,6 +7,17 @@ const proxy = "https://salty-ocean-03856.herokuapp.com/";
 const roomsSelect = document.querySelector("#rooms");
 const roomsForm = document.querySelector("#rooms-form");
 const reservationsUl = document.querySelector("#reservations-ul");
+const yesterdayButton = document.querySelector("#yesterday");
+const tomorrowButton = document.querySelector("#tomorrow");
+let selectedRoom;
+
+const date = new Date();
+
+let selectedDay = date;
+
+const splitDate = (date) => {
+  return date.toISOString().split("T")[0];
+};
 //hakee Karaportin kaikki huoneet
 const getRooms = async () => {
   const response = await fetch(
@@ -30,40 +41,57 @@ const getRooms = async () => {
   }
 };
 
-const getReservations = (today, room) => {
-  const date = new Date();
-  today = date.toISOString().split("T")[0];
-
-  roomsForm.addEventListener("submit", async (e) => {
-    reservationsUl.innerHTML = "";
-    e.preventDefault();
-    room = roomsSelect[roomsSelect.selectedIndex].value;
-    console.log(room);
-    const response = await fetch(proxy + url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: "Basic " + btoa("PNWqQ8p6R5sWevhU4Hu0:"),
-      },
-      body: `{\n   "startDate":"${today}T00:00",\n   "endDate":"${today}T23:59",\n   "room":["${room}"]\n}`,
-    });
-    const result = await response.json();
-
-    //   for (let i = 0; i < result.reservations.length; i++) {
-    //     console.table(result.reservations[i]);
-    //   }
-    for (const item of result.reservations) {
-      const li = document.createElement("li");
-      li.innerHTML = item.subject;
-      reservationsUl.appendChild(li);
-    }
-    console.log("getReservations", result);
+const getReservations = async (date, room) => {
+  const response = await fetch(proxy + url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa("PNWqQ8p6R5sWevhU4Hu0:"),
+    },
+    body: `{\n   "startDate":"${date}T00:00",\n   "endDate":"${date}T23:59",\n   "room":["${room}"]\n}`,
   });
+  const result = await response.json();
+
+  for (const item of result.reservations) {
+    const li = document.createElement("li");
+    li.innerHTML = item.subject;
+    reservationsUl.appendChild(li);
+  }
+  console.log("getReservations", result);
 };
 
-const date = new Date();
-const todayDate = date.toDateString();
-document.getElementById("today-date").innerHTML = todayDate;
+roomsForm.addEventListener("submit", (e) => {
+  reservationsUl.innerHTML = "";
+  e.preventDefault();
+  let room = roomsSelect[roomsSelect.selectedIndex].value;
+  selectedRoom = room;
+  console.log(room);
+  getReservations(splitDate(selectedDay), room);
+});
 
+yesterdayButton.onclick = () => {
+  const yesterday = new Date(date.setDate(date.getDate() - 1));
+  selectedDay = yesterday;
+
+  reservationsUl.innerHTML = "";
+
+  getReservations(splitDate(yesterday), selectedRoom);
+  displayDate(yesterday);
+};
+tomorrowButton.onclick = () => {
+  const tomorrow = new Date(date.setDate(date.getDate() + 1));
+  selectedDay = tomorrow;
+
+  reservationsUl.innerHTML = "";
+
+  getReservations(splitDate(tomorrow), selectedRoom);
+  displayDate(tomorrow);
+};
+
+const displayDate = (date) => {
+  document.getElementById("today-date").innerHTML = "";
+  const todayDate = date.toDateString();
+  document.getElementById("today-date").innerHTML = todayDate;
+};
+displayDate(date);
 getRooms();
-getReservations();
