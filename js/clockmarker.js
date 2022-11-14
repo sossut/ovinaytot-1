231b8d507c572a69058baf99f525456757322211
sossut <mikki.alakari@elisanet.fi>
2022-11-15 00:25:23 +0200

    liikkuva viiva

--- js/clockmarker.js
+++ js/clockmarker.js
@@ -1,25 +1,25 @@
 "use strict";
 const d = new Date();
-const hours = d.getHours();
-const minutes = d.getMinutes();
-let markerSpot;
-
 const containerHeight = document.querySelector(
   ".dayview-gridcell-container"
 ).clientHeight;
-if (hours < 7 || hours > 22) {
-  markerSpot = 0;
-} else {
-  markerSpot = (hours - 7 + minutes / 60) / 15;
-}
-console.log(containerHeight);
-console.log(markerSpot);
 
-if (hours < 7) {
-  document.querySelector(".dayview-now-marker").style.top = 0 + "px";
-} else if (hours > 22) {
-  document.querySelector(".dayview-now-marker").style.bottom = 1 + "px";
-} else {
-  document.querySelector(".dayview-now-marker").style.top =
-    containerHeight * markerSpot + "px";
-}
+const moveMarker = () => {
+  const plusSecond = new Date(d.setSeconds(d.getSeconds() + 1));
+
+  const hours = plusSecond.getHours();
+  const minutes = plusSecond.getMinutes();
+  const seconds = plusSecond.getSeconds();
+  if (hours < 7) {
+    document.querySelector(".dayview-now-marker").style.top = 0 + "px";
+  } else if (hours > 22) {
+    document.querySelector(".dayview-now-marker").style.bottom = 1 + "px";
+  } else {
+    document.querySelector(".dayview-now-marker").style.top =
+      (containerHeight * (hours - 7 + minutes / 60 + seconds / 3600)) / 15 +
+      "px";
+  }
+  console.log(plusSecond);
+};
+
+setInterval(moveMarker, 1000);
