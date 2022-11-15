17a937877f1fbcb598097b8286899c85c40d3f32
sossut <mikki.alakari@elisanet.fi>
2022-11-15 13:11:14 +0200

    turha

--- js/clockmarker.js
+++ js/clockmarker.js
@@ -9,17 +9,15 @@ const moveMarker = () => {
 
   const hours = plusSecond.getHours();
   const minutes = plusSecond.getMinutes();
-  const seconds = plusSecond.getSeconds();
+
   if (hours < 7) {
     document.querySelector(".dayview-now-marker").style.top = 0 + "px";
   } else if (hours > 22) {
     document.querySelector(".dayview-now-marker").style.bottom = 1 + "px";
   } else {
     document.querySelector(".dayview-now-marker").style.top =
-      (containerHeight * (hours - 7 + minutes / 60 + seconds / 3600)) / 15 +
-      "px";
+      (containerHeight * (hours - 7 + minutes / 60)) / 15 + "px";
   }
-  console.log(plusSecond);
 };
 
 setInterval(moveMarker, 1000);
