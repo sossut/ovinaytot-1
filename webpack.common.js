718da89d0b4496801e159f5fbea7aad3abe58b17
J-P Tuomanen <jukka-pekka.tuomanen@metropolia.fi>
2022-11-21 03:55:24 +0200

    pwa wörkkii

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+
+
 
 module.exports = {
   entry: {
@@ -30,6 +34,38 @@ module.exports = {
         collapseWhitespace: true,
       },
     }),
+
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+
+    new WebpackPwaManifest({
+      name: "Ovinäyttö",
+      id: "/",
+      publicPath: "/",
+      short_name: "oviPWA",
+      filename: "manifest.json",
+      description: "Ovinaytto PWA",
+      theme_color: "#F5F5F5",
+      background_color: "#F5F5F5",
+      icons: [
+        {
+          src: path.resolve('src/assets/img/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          type: "image/png",
+          purpose: "any"
+        },
+        {
+          src: path.resolve('src/assets/img/maskable_icon.png'),
+          size: "400x400",
+          type: "image/png",
+          purpose: "maskable"
+        },
+      ]
+
+    }),
+
     new ESLintPlugin({}),
   ],
   output: {
